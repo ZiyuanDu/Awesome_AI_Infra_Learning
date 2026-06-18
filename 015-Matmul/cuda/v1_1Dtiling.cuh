ffba183b95cd5ced79dcb7b1cf35d5fdd4af6c6d
doublesevenshop <1416642324@qq.com>
2026-06-19 00:59:24 +0800

    修改1DTiling

--- 015-Matmul/cuda/v1_1Dtiling.cuh
+++ 015-Matmul/cuda/v1_1Dtiling.cuh
@@ -1,54 +1,69 @@
 #pragma once
 #include <cuda_runtime.h>
 
-template <int BM = 64, int BN = 64, int BK = 4, int TM = 16>
+template <int BM = 128, int BN = 64, int BK = 4, int TM = 16>
 __global__ void sgemm_v1_1(const float* __restrict__ A,
                            const float* __restrict__ B,
                            float* __restrict__ C,
                            int M, int N, int K) {
                             
-  __shared__ float sA[BM][BK], sB[BK][BN];
+  __shared__ float SA[BM][BK];
+  __shared__ float SB[BK][BN];
 
-  const int bx = blockIdx.x, by = blockIdx.y;
-  const int tx = threadIdx.x, ty = threadIdx.y;
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
 
+  int col = blockIdx.x * BN + tx;
 
-  const int tid = ty * BN + tx;
-  const int col = bx * BN + tx;
-  float accum[TM] = {0.f};
+  float sum[TM] = {0.0f};
 
-  for (int bk = 0; bk < (K + BK - 1) / BK; ++bk) {
-    const int gk = bk * BK;
+  int tid = ty * blockDim.x + tx; 
 
-    // 加载矩阵A的元素到Shared
-    const int ar = tid / BK, ac = tid % BK;
-    const int ga_row = by * BM + ar, ga_col = gk + ac;
-    sA[ar][ac] = (ga_row < M && ga_col < K) ? A[ga_row * K + ga_col] : 0.f;
-    // 加载矩阵B的元素到Shared
-    const int br = tid / BN, bc = tid % BN;
-    const int gb_row = gk + br, gb_col = bx * BN + bc;
-    sB[br][bc] = (gb_row < K && gb_col < N) ? B[gb_row * N + bc + bx * BN] : 0.f;
+  for (int tile = 0; tile < K; tile += BK) {
+    
+    int load_a_row = tid / BK;
+    int load_a_col = tid % BK;
+    
+    // 全局A的坐标
+    int ga_row = blockIdx.y * BM + load_a_row;
+    int ga_col = tile + load_a_col;
+    
+    SA[load_a_row][load_a_col] = (ga_row < M && ga_col < K) ? A[ga_row * K + ga_col] : 0.0f;
+
+    int load_b_row = tid / BN;
+    int load_b_col = tid % BN;
+    
+    int gb_row = tile + load_b_row;            
+    int gb_col = blockIdx.x * BN + load_b_col;
+    
+    SB[load_b_row][load_b_col] = (gb_row < K && gb_col < N) ? B[gb_row * N + gb_col] : 0.0f;
+
     __syncthreads();
 
     
     #pragma unroll
-    for (int k = 0; k < BK; ++k) {
-      float b_val = sB[k][tx];
+    for (int k = 0; k < BK; k++) {
+      float b_val = SB[k][tx];
+      
       #pragma unroll
-      for (int m = 0; m < TM; ++m)
-        accum[m] += sA[ty * TM + m][k] * b_val;
+      for (int m = 0; m < TM; m++) {
+        sum[m] += SA[ty * TM + m][k] * b_val;
+      }
     }
     __syncthreads();
   }
 
   #pragma unroll
-  for (int m = 0; m < TM; ++m) {
-    int row = by * BM + ty * TM + m;
-    if (row < M && col < N) C[row * N + col] = accum[m];
+  for (int m = 0; m < TM; m++) {
+    int row = blockIdx.y * BM + ty * TM + m;
+    
+    if (row < M && col < N) {
+      C[row * N + col] = sum[m];
+    }
   }
 }
 
-template <int BM = 64, int BN = 64, int BK = 4, int TM = 16>
+template <int BM = 128, int BN = 64, int BK = 4, int TM = 16>
 inline void launch_sgemm_v1_1(const float* A, const float* B, float* C,
                               int M, int N, int K) {
   dim3 block(BN, BM / TM);                             
