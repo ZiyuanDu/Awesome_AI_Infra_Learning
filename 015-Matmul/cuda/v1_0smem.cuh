cfad8a5d89fd7b737477b8aa481a32d3c591b5f6
doublesevenshop <1416642324@qq.com>
2026-06-19 00:15:57 +0800

    修改1Dtiling

--- 015-Matmul/cuda/v1_0smem.cuh
+++ 015-Matmul/cuda/v1_0smem.cuh
@@ -8,13 +8,15 @@ __global__ void sgemm_v1(const float* A, const float* B, float* C, int M, int N,
   int ty = threadIdx.y;
   int tx = threadIdx.x;
   
+  // 以输出矩阵C为视角
   int row = blockIdx.y * BM + ty;
   int col = blockIdx.x * BN + tx;
   
   float sum = 0.f;
 
+  // A和B分别沿着K滑动，每次滑动长度为BK
   for (int k_tile = 0; k_tile < K; k_tile += BK) {
-    
+    // 从HBM放到Shared Memory中
     int ga = k_tile + tx;
     sA[ty][tx] = (row < M && ga < K) ? A[row * K + ga] : 0.f;
 
@@ -22,18 +24,18 @@ __global__ void sgemm_v1(const float* A, const float* B, float* C, int M, int N,
     sB[ty][tx] = (gb < K && col < N) ? B[gb * N + col] : 0.f;
     
     __syncthreads();
-    
+    // 从Shared memory中取值计算
     for (int k = 0; k < BK; ++k) {
       sum += sA[ty][k] * sB[k][tx];
     }
-    
     __syncthreads();
   }
-  
+  // 写回
   if (row < M && col < N) {
     C[row * N + col] = sum;
   }
 }
+
 inline void launch_sgemm_v1(const float* A, const float* B, float* C, int M, int N, int K) {
   dim3 block(32, 32);
   dim3 grid((N + 31) / 32, (M + 31) / 32);
--- 015-Matmul/cuda/v1_1Dtiling.cuh
+++ 015-Matmul/cuda/v1_1Dtiling.cuh
@@ -6,22 +6,25 @@ __global__ void sgemm_v1_1(const float* __restrict__ A,
                            const float* __restrict__ B,
                            float* __restrict__ C,
                            int M, int N, int K) {
+                            
   __shared__ float sA[BM][BK], sB[BK][BN];
 
   const int bx = blockIdx.x, by = blockIdx.y;
   const int tx = threadIdx.x, ty = threadIdx.y;
-  const int tid = ty * BN + tx;
 
+
+  const int tid = ty * BN + tx;
   const int col = bx * BN + tx;
   float accum[TM] = {0.f};
 
   for (int bk = 0; bk < (K + BK - 1) / BK; ++bk) {
     const int gk = bk * BK;
 
+    // 加载矩阵A的元素到Shared
     const int ar = tid / BK, ac = tid % BK;
     const int ga_row = by * BM + ar, ga_col = gk + ac;
     sA[ar][ac] = (ga_row < M && ga_col < K) ? A[ga_row * K + ga_col] : 0.f;
-
+    // 加载矩阵B的元素到Shared
     const int br = tid / BN, bc = tid % BN;
     const int gb_row = gk + br, gb_col = bx * BN + bc;
     sB[br][bc] = (gb_row < K && gb_col < N) ? B[gb_row * N + bc + bx * BN] : 0.f;
